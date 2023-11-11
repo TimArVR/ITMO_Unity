e2695aad2be5b50029e273c9b39ef3a732dc7fd7
TimArVR <tsposts@gmail.com>
2023-11-11 16:35:55 +0300

    multiplayer add joystics

--- Assets/Asteroids-Host-Simple/Input/LocalInputPoller.cs
+++ Assets/Asteroids-Host-Simple/Input/LocalInputPoller.cs
@@ -1,93 +1,100 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Fusion;
+using Asteroids.HostSimple;
 using Fusion.Sockets;
+using Fusion;
+using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-namespace Asteroids.HostSimple
+public class LocalInputPoller : MonoBehaviour, INetworkRunnerCallbacks
 {
-    public class LocalInputPoller : MonoBehaviour, INetworkRunnerCallbacks
+    private UISpaceShipInput _uiInput;
+
+    private const string AXIS_HORIZONTAL = "Horizontal";
+    private const string AXIS_VERTICAL = "Vertical";
+    private const string BUTTON_FIRE1 = "Fire1";
+    private const string BUTTON_JUMP = "Jump"; // Can be used as an alternative fire button to shoot with SPACE
+
+    // The INetworkRunnerCallbacks of this LocalInputPoller are automatically detected
+    // because the script is located on the same object as the NetworkRunner and
+    // NetworkRunnerCallbacks scripts.
+
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
     {
-        private const string AXIS_HORIZONTAL = "Horizontal";
-        private const string AXIS_VERTICAL = "Vertical";
-        private const string BUTTON_FIRE1 = "Fire1";
-        private const string BUTTON_JUMP = "Jump"; // Can be used as an alternative fire button to shoot with SPACE
+        if (_uiInput == null) _uiInput = FindObjectOfType<UISpaceShipInput>();
+        if (_uiInput == null) return;
 
-        // The INetworkRunnerCallbacks of this LocalInputPoller are automatically detected
-        // because the script is located on the same object as the NetworkRunner and
-        // NetworkRunnerCallbacks scripts.
-
-        public void OnInput(NetworkRunner runner, NetworkInput input)
-        {
             SpaceshipInput localInput = new SpaceshipInput();
 
-            localInput.HorizontalInput = Input.GetAxis(AXIS_HORIZONTAL);
-            localInput.VerticalInput = Input.GetAxis(AXIS_VERTICAL);
-            localInput.Buttons.Set(SpaceshipButtons.Fire, Input.GetButton(BUTTON_FIRE1));
+        localInput.HorizontalInput = _uiInput.Horizontal;
+        localInput.VerticalInput = _uiInput.Vertical;
+        localInput.Buttons.Set(SpaceshipButtons.Fire, _uiInput.NeedFire);
 
-            input.Set(localInput);
-        }
+        //localInput.HorizontalInput = Input.GetAxis(AXIS_HORIZONTAL);
+        //localInput.VerticalInput = Input.GetAxis(AXIS_VERTICAL);
+        //localInput.Buttons.Set(SpaceshipButtons.Fire, Input.GetButton(BUTTON_FIRE1));
+        input.Set(localInput);
+        _uiInput.InputReaded();
+    }
 
-        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
-        {
-        }
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+    }
 
-        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
-        {
-        }
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+    }
 
-        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
-        {
-        }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+    }
 
-        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
-        {
-        }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+    }
 
-        public void OnConnectedToServer(NetworkRunner runner)
-        {
-        }
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+    }
 
-        public void OnDisconnectedFromServer(NetworkRunner runner)
-        {
-        }
+    public void OnDisconnectedFromServer(NetworkRunner runner)
+    {
+    }
 
-        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request,
-            byte[] token)
-        {
-        }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request,
+        byte[] token)
+    {
+    }
 
-        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-        {
-        }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+    }
 
-        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
-        {
-        }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+    }
 
-        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
-        {
-        }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+    }
 
-        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
-        {
-        }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+    }
 
-        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
-        {
-        }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+    }
 
-        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
-        {
-        }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
+    {
+    }
 
-        public void OnSceneLoadDone(NetworkRunner runner)
-        {
-        }
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+    }
 
-        public void OnSceneLoadStart(NetworkRunner runner)
-        {
-        }
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
     }
 }
---
+++ Assets/Asteroids-Host-Simple/Input/UISpaceShipInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UISpaceShipInput : MonoBehaviour
+{
+    [SerializeField] private Joystick joystick;
+    [SerializeField] private Button fireButton;
+
+    public float Horizontal => joystick.Horizontal;
+    public float Vertical => joystick.Vertical;
+    public bool NeedFire => _needFireRequested;
+
+    private bool _needFireRequested;
+
+    private void OnEnable()
+    {
+        fireButton.onClick.AddListener(OnFire);
+    }
+
+    private void OnDisable()
+    {
+        fireButton.onClick.RemoveListener(OnFire);
+    }
+
+    private void OnFire()
+    {
+        _needFireRequested = true;
+    }
+
+    public void InputReaded()
+    {
+        _needFireRequested = false;
+    }
+}
